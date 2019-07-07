ba38f8abc361209b8bbb7a40cf9a7ff533fded8c
Cam McLeod <cammcleod3@gmail.com>
2019-07-07 14:05:58 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Cameron Mcleod on 2019-07-07.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
